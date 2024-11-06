5beca4f80c2ef69e04bd17cf6198f9e52b749e5a
Rebecca Otero <iotero07@gmail.com>
2024-11-06 12:49:12 -0500

    String Practice

---
+++ script.js
@@ -0,0 +1,16 @@
+"use stricts";
+
+let userName = prompt("What is you name?"); //If I type in "Rebecca"
+
+// "Hola", "+ "Rebecca" + " Welcome back"
+let welcomeMessage = "Hola, "+ userName + "Welcome Back!"
+
+console.log(welcomeMessage);
+console.log(welcomeMessage);
+console.log(welcomeMessage);
+console.log(welcomeMessage);
+
+
+
+
+document.getElementById("string").innerText + welcomeMessage;
